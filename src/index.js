f03a2ec2c23628c53d05a9b5c6cfd16e5b08c8ca
lel <daniil.tsimbalyuk@gmail.com>
2022-01-02 18:50:15 -0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
-const {Menu, app, BrowserWindow} = require('electron')
-const path = require('path')
+const {Menu, app, BrowserWindow} = require('electron');
+const path = require('path');
+const fetch = require('node-fetch');
+
+async function getVersion() {
+	const url = 'https://api.github.com/repos/Iamlel/ICE/releases/latest';
+	const response = await fetch(url);
+	const result = await response.json();
+	return ((result.tag_name == undefined) ? "<Unknown>": result.tag_name);
+}
 
 const createNotepad = () => {
 	const notepad = new BrowserWindow({	
-		width: 1420,
+		width: 720,
 		height: 1020,
 		icon: path.join(__dirname, '../', "temporary.ico"),
 	});
+	getVersion().then(x => { notepad.setTitle("ICE " + x); });
 	notepad.loadFile(path.join(__dirname, "index.html"));
 	createMenu(notepad);
 }
 
 const createEditor = () => {
-	//const fileEditor = new BrowserWindow({ width: 700, height: 700, });
+	
 }
 
 const createMenu = (notepad) => {
@@ -20,6 +29,15 @@ const createMenu = (notepad) => {
 		{
 			label: 'File',
 			submenu: [
+			{ label: 'New' },
+			{ label: 'Open…' },
+			{ type: 'separator' },
+			{ label: 'Save' },
+			{ label: 'Save As…' },
+			{ label: 'Save a Copy As…' },
+			{ label: 'Save All' },
+			{ label: 'Rename…' },
+			{ type: 'separator' },
 			{ role: 'quit' }
 			]
 		},
@@ -32,7 +50,6 @@ const createMenu = (notepad) => {
 			{ role: 'cut' },
 			{ role: 'copy' },
 			{ role: 'paste' },
-			{ role: 'pasteandmatchstyle' },
 			{ role: 'delete' },
 			{ role: 'selectall' }
 			]
@@ -99,44 +116,3 @@ app.on('activate', () => {
 		createWindow();
 	}
 });
-//
-//function createWindow () {
-//  // Create the browser window.
-//  const mainWindow = new BrowserWindow({
-//    width: 800,
-//    height: 600,
-//    webPreferences: {
-//      preload: path.join(__dirname, 'preload.js')
-//    }
-//  })
-//
-//  // and load the index.html of the app.
-//  mainWindow.loadFile('./index.html')
-//
-//  // Open the DevTools.
-//  // mainWindow.webContents.openDevTools()
-//}
-//
-//// This method will be called when Electron has finished
-//// initialization and is ready to create browser windows.
-//// Some APIs can only be used after this event occurs.
-//app.whenReady().then(() => {
-//  createWindow()
-//
-//  app.on('activate', function () {
-//    // On macOS it's common to re-create a window in the app when the
-//    // dock icon is clicked and there are no other windows open.
-//    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-//  })
-//})
-//
-//// Quit when all windows are closed, except on macOS. There, it's common
-//// for applications and their menu bar to stay active until the user quits
-//// explicitly with Cmd + Q.
-//app.on('window-all-closed', function () {
-//  if (process.platform !== 'darwin') app.quit()
-//})
-//
-//// In this file you can include the rest of your app's specific main process
-//// code. You can also put them in separate files and require them here.
-//
